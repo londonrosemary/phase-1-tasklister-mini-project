b6d90225eb6d8e96adf2aabc5e676b4e91f71d75
London <london.rosemary@gmail.com>
2021-08-17 19:06:08 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    renderTask(e.target.querySelector("input").value);
+    newTaskForm.reset();
+  })
+  
 });
+
+function renderTask(userInput) {
+  const newItem = document.createElement("li");
+  const delBttn = document.createElement("button");
+
+  newItem.textContent = `${userInput} `;
+  delBttn.addEventListener("click", () => newItem.remove());
+
+  tasks.appendChild(newItem);
+  delBttn.textContent = " del ";
+  newItem.appendChild(delBttn);
+}
